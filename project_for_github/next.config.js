e002cbf04486de9f3f9279570bf1fb3a8ba7fd8a
IlyaKarakcheev <43365825+IlyaKarakcheev@users.noreply.github.com>
2026-07-11 19:22:05 +0300

    Delete project_for_github directory

--- project_for_github/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Отключаем строгие проверки для Outlook Add-in, если это необходимо
-};
-
-module.exports = nextConfig;
